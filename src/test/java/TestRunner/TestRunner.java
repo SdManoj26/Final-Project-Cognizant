a9209933e5a71492675674c04a540fb6cf55a6a8
Manoj <sdmanoj26@gmail.com>
2024-06-17 14:44:45 +0530

    removed testng

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -1,19 +1,19 @@
 package TestRunner;
 
+import org.junit.runner.RunWith;
 
-import org.testng.annotations.Test;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;
 
-@Test
+
+@RunWith(Cucumber.class)
 @CucumberOptions
 	(features = ".//Features/hackathon.feature" ,
 	glue = {"Hooks", "StepsDefinition"},
 	plugin = {"pretty", "html:Reports/Cucumber/cucumberReport.html",
 			"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}	
 	)
-public class TestRunner extends AbstractTestNGCucumberTests{
+public class TestRunner{
 
 }
